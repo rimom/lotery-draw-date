906b8dce0e59f1e4a10195d315aa3681ef8d6c05
Rimom Costa <46904592+rimom@users.noreply.github.com>
2019-01-22 22:02:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$date = new DateTime(); 
+$date = new DateTime(); //<-entrypoint
+$lotteryCalculator = new LotteryCalculator($date);
+$nextDay = $lotteryCalculator->getNextDrawDate();
+
+echo $nextDay->format('d-M-Y');
+echo PHP_EOL;
+
+$date = new DateTime('2019-01-09 19:59:59'); //<-entrypoint
 $lotteryCalculator = new LotteryCalculator($date);
 $nextDay = $lotteryCalculator->getNextDrawDate();
 
